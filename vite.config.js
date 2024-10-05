85713098a15916df3723f64690cd5d89c2d08487
Ke Jia <ke.jia@metropolia.fi>
2024-10-05 04:20:35 +0300

    fix for vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(import.meta.env.VITE_PORT) || 3000,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_PROXY_TARGET || 'http://localhost:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  },
+  //server: {
+    //port: parseInt(import.meta.env.VITE_PORT) || 3000,
+    //proxy: {
+      //'/api': {
+        //target: import.meta.env.VITE_API_PROXY_TARGET || 'http://localhost:4000',
+        //changeOrigin: true,
+        //rewrite: (path) => path.replace(/^\/api/, ''),
+      //},
+    //}
+  //},
 })
 
 
